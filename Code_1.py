c660ea30529ccd43760f46816a17327592d6c4a8
ming197 <ming197@qq.com>
2020-08-03 21:03:07 +0800

    Update!

--- Code_1.py
+++ Code_1.py
@@ -9,41 +9,90 @@ from sklearn import metrics
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-import math
-
-filename = "blogData_train.csv"
-train_data = pd.read_csv(filename,header=None)
-#train_data = train_data.iloc[np.random.permutation(len(train_data))]
-train_output = train_data[len(train_data.columns)-1]
-del train_data[len(train_data.columns)-1]
-
-filename = "blogData_test-2012.02.01.00_00.csv"
-test_data = pd.read_csv(filename,header=None)
-test_output = test_data[len(test_data.columns)-1]
-del test_data[len(test_data.columns)-1]
-rank_test = [index for index,value in sorted(list(enumerate(test_output)),key=lambda x:x[1], reverse=True)]
-
-reg = LinearRegression()
-rf = RandomForestRegressor()
-gradBoost = GradientBoostingRegressor()
-ada = AdaBoostRegressor()
+from sklearn.ensemble import BaggingRegressor
+import math, os, random 
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_validate
 
 
+# Plot the learning curve
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+  
+    if axes is None:
+        _, axes = plt.subplots(1, 1, figsize=(20, 5))
 
-regressors = [reg,rf,gradBoost,ada]
-regressor_names = ["Linear Regression","Random Forests","Gradient Boosting","Adaboost"]
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    # fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot fit_time vs score
+    axes[0].grid()
+    axes[0].fill_between(fit_times_mean, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(fit_times_mean, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(fit_times_mean, test_scores_mean, 'o-', color="g",
+                 label="Test score")
+    axes[0].legend(loc="best")
+
+    return plt
 
 
-
-for regressor,regressor_name in zip(regressors,regressor_names):
+# Load the train data and test data
+def load_data():
     
-    regressor.fit(train_data,train_output)
-    predicted_values = regressor.predict(test_data)
-    rank_predict = [index for index,value in sorted(list(enumerate(predicted_values)),key=lambda x:x[1], reverse=True)]
-    counter = len([x for x in rank_predict[:10] if x in rank_test[:10]])
+    files = os.listdir("Data")
+    filename = files[-1]
+    train_data = pd.read_csv("./Data/{}".format(filename),header=None)
+    train_output = train_data[len(train_data.columns)-1]
+    train_num = train_data.shape[0]
+    del train_data[len(train_data.columns)-1]
 
-    print ("Mean Squared Error for ",regressor_name, " : ", metrics.mean_squared_error(test_output,predicted_values))
-    print ("R2 score for ",regressor_name, " : ",metrics.r2_score(test_output,predicted_values))
-    print ("HIT@10 for ",regressor_name, " : ",counter)
-    print("\n")
+    file_list = files[:-1]
+    test_data = pd.DataFrame()
+    for filename in file_list:
+        df = pd.read_csv("./Data/{}".format(filename),header=None)
+        test_data = pd.concat([test_data, df], axis=0)
+    test_output = test_data[len(test_data.columns)-1]
+    del test_data[len(test_data.columns)-1]
 
+    data_X = pd.concat([train_data, test_data], axis=0)
+    data_Y = pd.concat([train_output, test_output], axis=0)
+
+    return data_X, data_Y, train_num
+
+
+#  define the models,which are baggings of MLP, KNN, RBF Network, Model Tree(M5P, REF), Linear Regression
+def models():
+    # Initialize the models
+    linear_reg = LinearRegression()
+
+    regressors = [linear_reg]
+    regressor_names = ["Linear Regression"]
+
+    # Bagging of the models
+    regressors_bagging = []
+    for reg in regressors:
+
+        regEnsemble = BaggingRegressor(base_estimator=reg, n_estimators=10, max_samples=1.0, oob_score=True, random_state=random.randint(0,100))
+    regressors.append(regEnsemble)
+    regressor_names.append("Linear Regression Ensemble")
